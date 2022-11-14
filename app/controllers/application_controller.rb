bc4efd38dabfee0bf764707c4a897fd24c41d090
ricardomendes <ricardogaditmendes26@gmail.com>
2022-11-14 14:27:41 +0000

    exercise 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  def ask
+  end
+
+  def answer
+    if params[:question] == "Hello"
+      @answer = "hello there"
+    elsif params[:question] == "what is the time?"
+      @answer = Time.now
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
